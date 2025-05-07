fe80de7f0af3c24deadfdf9b48f1a18b88394c2a
Super Abel <superabel000@gmail.com>
2025-05-07 17:50:46 +0100

    Add CORS middleware to handle cross-origin requests

---
+++ api/app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept, Authorization');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Max-Age', '86400');
+
+        if ($request->isMethod('OPTIONS')) {
+            $response->setStatusCode(200);
+        }
+
+        return $response;
+    }
+}
